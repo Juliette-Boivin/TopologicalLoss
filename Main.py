9ce8502a0db6076e528c022d192e3b51bbc46875
Juliette-Boivin <85223936+Juliette-Boivin@users.noreply.github.com>
2021-06-10 11:12:42 +0200

    references to parameters entered by the user

--- Main.py
+++ Main.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from Experiment import experiment
+from Arguments import get_args
 import TestData
 
-
-graph, image = TestData.testdata["multi_max_noisy"]
-experiment(graph, image, nb_iter=10, loss='minmax', nb_cc=2, thresh_max=1e-5, thresh_min=1e-10)
-
+args = get_args()
+grapp, imagg = TestData.testdata["multi_max_noisy"]
+experiment(grapp, imagg, args.nb_iter, args.loss, args.nb_holes, args.nb_cc, args.thresh_min, args.thresh_max, 
+           args.perf_dgm, args.img_grph_GT, args.optimize_maxtree, args.color_min, args.color_max, args.plot_loss, 
+           args.result, args.diag_beg, args.diag_end, args.nb_leaves, args.save_fig, args.save_diagrams, args.save_results)
 
 
 #import edist
